92c3d57e962e791ddf80f8a8f7c047978980ff35
nikhilxifer <nikhs286@gmail.com>
2021-06-01 16:55:42 +0530

    c

--- bloodbankmanagement/urls.py
+++ bloodbankmanagement/urls.py
@@ -25,8 +25,7 @@ urlpatterns = [
 
 
 
-     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    
     
     
     
