da8edcb21a330148f4b4917a0ce6ff98c141b4a6
nikhilxifer <nikhs286@gmail.com>
2021-06-01 18:54:33 +0530

    Update settings.py

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -15,8 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_DIR='/static/'
+MEDIA_ROOT='/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -131,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+
 
 STATICFILES_DIRS=[
 STATIC_DIR,
